7a60fc523b42d3ac8afdc4613483adbdb048dd11
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-03-16 00:59:18 +0100

    Adding i18n to web app

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#home'
+  scope ":locale", locale: /es|en/ do
+    root 'pages#home'
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
