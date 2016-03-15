3bf7c65a5288257004d0d2b65b7b2a5fff6b3b61
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-03-15 23:49:42 +0100

    Making login and sign up work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,11 +6,14 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Devise config
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
