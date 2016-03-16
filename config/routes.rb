e8e75d2b95cde00f505346a74810107277094987
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-03-16 13:13:16 +0100

    Adding translations to views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  scope ":locale", locale: /es|en/ do
+  scope "/:locale", locale: /es|en/ do
     root 'pages#home'
   end
 
