07e03819913579c5418b3c5c9f1e1781aa8d424f
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-04-19 23:34:23 +0200

    Form add category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,23 @@ class CategoriesController < ApplicationController
     @categories = Category.all
     @category = Category.find_by(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = t('category_created')
+      redirect_to root_path
+    else
+      flash[:alert] = t('error')
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end
