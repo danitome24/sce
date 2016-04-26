74b77c45a0df3581920745c1898214f13ddeb049
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-04-26 19:03:44 +0200

    Upgrading link to products in admin part

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,4 +2,9 @@ class AdminsController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    @products = @category.products.with_translations(I18n.locale)
+  end
 end
