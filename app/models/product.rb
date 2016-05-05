11f22cf3a0e3687e019eae4db76ae2965f704a8a
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-05-05 17:39:40 +0200

    Update models with relations and model functions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :order_items
+
+  default_scope { where(active: true) }
   translates :name, :description
   globalize_accessors :locales => I18n.available_locales, :attributes => [:name, :description]
   belongs_to :category
