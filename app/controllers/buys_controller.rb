ff932f17609d5bec8e666f33a40c4c7a418df0d4
Daniel Tomé Fernández <danieltomefer@gmail.com>
2016-05-29 11:14:44 +0200

    Update buys controller

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,12 +1,13 @@
 class BuysController < ApplicationController
   def express_checkout
-    response = EXPRESS_GATEWAY.setup_purchase(2000,
+    @order = Order.find_by(client_id: current_user.id)
+    response = EXPRESS_GATEWAY.setup_purchase(@order.subtotal*100,
                                               ip: request.remote_ip,
-                                              return_url: 'http://bba2923d.ngrok.io/es/checkout_details',
-                                              cancel_return_url: 'http://bba2923d.ngrok.io/es',
+                                              return_url: 'http://8e361085.ngrok.io/es/checkout_details',
+                                              cancel_return_url: 'http://8e361085.ngrok.io/es',
                                               currency: 'EUR',
                                               allow_guest_checkout: true,
-                                              items: [{name: 'Order', description: '"Order  description', quantity: '1', amount: 2000}]
+                                              items: [{name: 'Order', description: '"Order  description', quantity: '1', amount: @order.subtotal*100}]
     )
 
     redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
@@ -19,12 +20,16 @@ class BuysController < ApplicationController
   end
 
   def purchase
+    @order = Order.find_by(client_id: current_user.id)
     @tok = params[:token]
     @payer = params[:PayerID]
-    @result = EXPRESS_GATEWAY.purchase(2000, {
+    @result = EXPRESS_GATEWAY.purchase(@order.subtotal*100, {
         :token => @tok,
         :payer_id => @payer,
         :currency => 'EUR'
     })
+    if @result.success?
+      Order.find_by(client_id: current_user.id).update(order_status_id: 3)
+    end
   end
 end
